132f84eee9989b763bef0320c2bda6ca181d7555
jamshedGit <153909482+jamshedGit@users.noreply.github.com>
2024-09-03 14:41:03 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -12,31 +12,10 @@ try {
     port: 3306,
     dialect: "mysql",
     logging:false,
-  
-    
-    // Localhost Database Connection
-    // database: "EAMS2",
-    // username: "postgres",
-    // password: "postgresql",
-    // host: "localhost",
-    // port: 5432,
-    // dialect: "postgres",
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true, // This will help you. But you will see new error
-    //     rejectUnauthorized: false, // This line will fix new error
-    //   },
-    // },
     define: {
-      // timestamps: true,
       freezeTableName: true,
     },
-    // host: envVars.HOST,
-    // port: envVars.DB_PORT,
-    // database: envVars.DATABASE,
-    // dialect: envVars.DIALECT,
-    // username: envVars.USERNAME,
-    // password: envVars.PASSWORD
+  
   });
 
   sequelize.authenticate();
