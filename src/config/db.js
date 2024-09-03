63782034a867473ed4d5efa1612f0ff2e7b49dab
jamshedGit <153909482+jamshedGit@users.noreply.github.com>
2024-09-03 12:37:57 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,34 +4,15 @@ const logger = require("./logger");
 var sequelize;
 try {
   sequelize = new Sequelize({
-    // Deploy Databse Connection
-    // database: "d7qc8bukkodtsn",
-    //     username: "rzlxhcjausmazc",
-    //     password: "ebbd35720be2186b3718b7c56a124cfd50733e290cd331db062db01017c3faff",
-    //     host: "ec2-3-219-52-220.compute-1.amazonaws.com",
-    // database: "EAMS_LIVE_DB",
-    // username: "postgres", //Arsal1997   arsalans credentials
-    // password: "Dynasoft@1", //pSlC3yjUvd5t
-    // host: "localhost", //ep-little-math-774305.us-east-2.aws.neon.tech
-    // port: 5432, 
-    // dialect: "postgres",
-
-    // database: "EAMS_LIVE_DB",
-    // username: "postgres", //Arsal1997   arsalans credentials
-    // password: "Dynasoft@1", //pSlC3yjUvd5t
-    // host: "localhost", //ep-little-math-774305.us-east-2.aws.neon.tech
-    // port: 5432,
-    // dialect: "postgres",
-
+    
     database: "sql10729008",
-    username: "sql10729008", //Arsal1997   arsalans credentials
-    password: "sMYzNnscZT", //pSlC3yjUvd5t
-    host: "sql10.freemysqlhosting.net", //ep-little-math-774305.us-east-2.aws.neon.tech
+    username: "sql10729008", 
+    password: "sMYzNnscZT", 
+    host: "sql10.freemysqlhosting.net",
     port: 3306,
     dialect: "mysql",
-
     logging:false,
-    dialectOptions: {},
+  
     
     // Localhost Database Connection
     // database: "EAMS2",
