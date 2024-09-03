cab7766c3da4d71010c48b32d8250b439c76f144
jamshedGit <153909482+jamshedGit@users.noreply.github.com>
2024-09-03 17:14:37 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,49 @@ const logger = require("./logger");
 var sequelize;
 try {
   sequelize = new Sequelize({
-    
+
     database: "sql10729008",
-    username: "sql10729008", 
-    password: "sMYzNnscZT", 
+    username: "sql10729008",
+    password: "sMYzNnscZT",
     host: "sql10.freemysqlhosting.net",
     port: 3306,
     dialect: "mysql",
-    logging:false,
+    logging: false,
+    define: {
+      freezeTableName: true,
+    },
+
+    // database: "dsc_hrms_staging",
+    // username: "root", //Arsal1997   arsalans credentials
+    // password: "", //pSlC3yjUvd5t
+    // host: "localhost", //ep-little-math-774305.us-east-2.aws.neon.tech
+    // port: 3306,
+    // dialect: "mysql",
+    // logging: true,
+    // dialectOptions: {
+    //   // ssl: {
+    //   //   require: false, // This will help you. But you will see nwe error
+    //   //   rejectUnauthorized: false // This line will fix new error
+    //   // }
+    // },
+
+
+
+
+    //================================
+    // Localhost Database Connection
+    // database: "EAMS2",
+    // username: "postgres",
+    // password: "postgresql",
+    // host: "localhost",
+    // port: 5432,
+    // dialect: "postgres",
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true, // This will help you. But you will see new error
+    //     rejectUnauthorized: false, // This line will fix new error
+    //   },
+    // },
     define: {
       freezeTableName: true,
     },
