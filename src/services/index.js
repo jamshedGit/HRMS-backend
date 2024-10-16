8b7019b7f3825459235649c03e314a8a72022c8e
Muhammad Ahmed <ahmed.habib@dynasoftcloud.com>
2024-10-16 16:21:07 +0500

    remove spaces

--- src/services/index.js
+++ src/services/index.js
@@ -45,11 +45,3 @@ module.exports.salarypolicyFormService = require('./operations/salarypolicy/sala
 module.exports.OneTimeEarningService = require('./operations/onetime_allowance/onetime_allowance.service')
 module.exports.LoanTypeService = require('./operations/loan_type/loan_type.service')
 module.exports.PayrollProcessPolicy = require('./operations/payroll_process_policy/payroll_process_policy.service')
-  
- 
-
-
-
-
-
-
