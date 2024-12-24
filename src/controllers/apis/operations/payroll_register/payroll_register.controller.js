d66cd6f8f1a0486826c9bd7f1ac35a19c5ef2290
Muhammad Ahmed <ahmed.habib@dynasoftcloud.com>
2024-12-24 17:04:28 +0500

    Payroll Register Form

---
+++ src/controllers/apis/operations/payroll_register/payroll_register.controller.js
@@ -0,0 +1,43 @@
+const catchAsync = require("../../../../utils/catchAsync");
+const { PayrollRegisterServicePage } = require("../../../../services/index");
+const {
+  HttpStatusCodes,
+  HttpResponseMessages,
+} = require("../../../../utils/constants");
+
+/**
+ * 
+ * Get All Registered Leave Application with Pagination
+ * 
+ * @param {Object} req 
+ * @returns res
+ */
+const getAllRegisteredLeaves = catchAsync(async (req, res) => {
+  const data = await PayrollRegisterServicePage.getAllRegisteredLeaves(req);
+  res.send({
+    code: HttpStatusCodes.OK,
+    message: HttpResponseMessages.OK,
+    data: data
+  });
+});
+
+/**
+ * 
+ * Get All Registered Leave Application for Pdf
+ * 
+ * @param {Object} req 
+ * @returns res
+ */
+const getAllRegisteredLeavesPdfData = catchAsync(async (req, res) => {
+  const data = await PayrollRegisterServicePage.getAllRegisteredLeavesForPdf(req);
+  res.set({
+    "Content-Type": "application/pdf",
+    "Content-Disposition": 'attachment; filename="kamil_test.pdf"',
+  });
+  res.end(data);
+});
+
+module.exports = {
+  getAllRegisteredLeaves,
+  getAllRegisteredLeavesPdfData
+};
--- src/routes/apis/index.js
+++ src/routes/apis/index.js
@@ -68,6 +68,7 @@ const payroll_process=require('./operations/payroll_process/payroll_process.rout
 const attendance=require('./operations/attendance/attendance.route')
 const employee_roster = require('./operations/employee_roster/employee_roster.route');
 const leave_register = require('./operations/leave_register/leave_register.route');
+const payroll_register = require('./operations/payroll_register/payroll_register.route');
 
 const router = express.Router();
 
@@ -335,6 +336,10 @@ const defaultRoutes = [
   {
     path: "/leave_register",
     route: leave_register
+  },
+  {
+    path: "/payroll_register",
+    route: payroll_register
   },
 
 ];
