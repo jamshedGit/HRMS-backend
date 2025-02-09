8656d72eb20470bc005a0dadfefa2d19867195f8
Faaiz <faaiz@gmail.com>
2025-02-09 19:33:17 +0500

    tax done

--- src/services/operations/payroll_process/payroll_process.service.js
+++ src/services/operations/payroll_process/payroll_process.service.js
@@ -88,7 +88,17 @@ const createPayroll_Process = async (req, payroll_processBody) => {
         addedPayroll_Process.completed = 1;
         await addedPayroll_Process.save();
       }
+      let TaxCalculated = await sequelize.query(
+        'SELECT COUNT(1) AS COUNT FROM t_Payroll_IncomeTax WHERE SubsidiaryId = :SubsidiaryId AND PayrollGroupId = :PayrollGroupId AND MonthId = :MonthId AND TaxfortheMonth > 0',
+        {
+          replacements: { SubsidiaryId:payroll_processBody?.subsidiaryId, PayrollGroupId: payroll_processBody?.payroll_groupId, MonthId:payroll_processBody?.payroll_monthId },
+          type: sequelize.QueryTypes.SELECT
+        }
+      );
 
+
+
+      result[0].TaxCalculated = TaxCalculated[0].COUNT;
       return result[0]
 
 
@@ -111,7 +121,18 @@ const createPayroll_Process = async (req, payroll_processBody) => {
               type: Sequelize.QueryTypes.RAW // Use RAW type for executing stored procedures
             });
 
-            final_result.TaxCalculated += result[0].TaxCalculated;
+      
+            let TaxCalculated = await sequelize.query(
+              'SELECT COUNT(1) AS COUNT FROM t_Payroll_IncomeTax WHERE SubsidiaryId = :SubsidiaryId AND PayrollGroupId = :PayrollGroupId AND MonthId = :MonthId AND TaxfortheMonth > 0',
+              {
+                replacements: { SubsidiaryId:payroll_processBody?.subsidiaryId, PayrollGroupId: payrollGroup?.Id, MonthId:payroll_processBody?.payroll_monthId },
+                type: sequelize.QueryTypes.SELECT
+              }
+            );
+
+
+
+            final_result.TaxCalculated += TaxCalculated[0].COUNT;
             final_result.TaxNotCalculated += result[0].TaxNotCalculated;
             final_result.LoanProcess += result[0].LoanProcess;
             final_result.EmployeewithZeroSalary += result[0].EmployeewithZeroSalary;
@@ -123,6 +144,9 @@ const createPayroll_Process = async (req, payroll_processBody) => {
         }
       }
 
+      // (SELECT Count(1) FROM t_Payroll_IncomeTax WHERE EmpId = x.EmpId AND PayrollGroupId = p_PayrollGroupId AND MonthId = p_MonthId AND TaxfortheMonth > 0) TaxCalculated,
+   
+     
       return final_result
 
     }
